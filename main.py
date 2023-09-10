3a8e147da4a162b828f0c7a48be1f5f248f88945
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-10 17:34:18 -0600

    Getting the letters and the names and combine the letters with the names, so we can add the name to the string.

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+names = []
+invites = []
+with open("Input/Names/invited_names.txt") as name:
+    for person in name:
+        names.append(person.replace('\n', ' '))
+
+print(names)
+
+
+with open("Input/Letters/starting_letter.txt") as letter:
+    content = letter.read()
+    for name in names:
+        invite = content.replace('[name]', name)
+        invites.append(invite)
+
+for invite in invites:
+    with open(f"Output/ReadyToSend/example.txt", mode="w") as file:
+        final = file.write(invite)
+
+
